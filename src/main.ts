aa4faf89126715f9f71f01d850b740915227ab4e
Graciana Baratti <graciana@parozlabs.com>
2025-06-20 12:25:23 -0300

    Reemplazar body-parser por express para el manejo de solicitudes en main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as bodyParser from 'body-parser';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
 
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('❌ Error al iniciar la aplicación:', err);
+});
